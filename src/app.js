703df384aa660017a108872dc465e8f69ce83e66
Jakub Nowak <jakub.nowak04@icloud.com>
2024-11-28 21:38:12 +0100

    Add generating book element and add it to UI.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+//Selected elements
+const libraryEl = document.querySelector(".library");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -25,6 +28,75 @@ function getBooksFromLibrary() {
   }
 }
 
+function createBookElement(book) {
+  const bookContainer = document.createElement("div");
+  bookContainer.className = "book";
+
+  const bookImgContainer = document.createElement("div");
+  bookImgContainer.className = "book-img";
+  const img = document.createElement("img");
+  img.src = "./test.jpg"; // Placeholder image source
+  img.alt = book.title;
+  bookImgContainer.appendChild(img);
+
+  const bookInfo = document.createElement("div");
+  bookInfo.className = "book-info";
+
+  const heading = document.createElement("h3");
+  heading.textContent = book.title;
+  const author = document.createElement("p");
+  author.textContent = book.author;
+  const pages = document.createElement("p");
+  pages.textContent = `Pages: ${book.pages}`;
+
+  const label = document.createElement("label");
+  label.htmlFor = "read-book";
+  label.textContent = "Read: ";
+
+  const select = document.createElement("select");
+  select.name = "read";
+  select.id = "read-book";
+
+  const optionYes = document.createElement("option");
+  optionYes.value = "true";
+  optionYes.textContent = "Yes";
+
+  const optionNo = document.createElement("option");
+  optionNo.value = "false";
+  optionNo.textContent = "No";
+
+  select.appendChild(optionYes);
+  select.appendChild(optionNo);
+  select.value = book.read ? "true" : "false";
+
+  label.appendChild(select);
+
+  const btn = document.createElement("button");
+  btn.className = "book-btn";
+  btn.textContent = "Remove";
+
+  bookInfo.appendChild(heading);
+  bookInfo.appendChild(author);
+  bookInfo.appendChild(pages);
+  bookInfo.appendChild(label);
+  bookInfo.appendChild(btn);
+
+  bookContainer.appendChild(bookImgContainer);
+  bookContainer.appendChild(bookInfo);
+
+  return bookContainer;
+}
+
+function generateLibrary() {
+  myLibrary.forEach((book) => {
+    const bookEl = createBookElement(book);
+    //Book added to UI
+    libraryEl.appendChild(bookEl);
+  });
+}
+
 //Example Data
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
+
+generateLibrary();
